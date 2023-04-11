6f23295286232f48ccc7b442eef9977f5f7556c5
CamiloVega2 <vegajuan208@gmail.com>
2023-04-11 13:44:43 -0500

    we

--- components/card.vue
+++ components/card.vue
@@ -1,11 +1,27 @@
 <template >
-    <div class="w-[18%] h-[1/2] bg-gray-200">
-        <img src="https://pokeapi.co/api/v2/">
+    <div class="w-[18%] h-[1/2] bg-gray-200 flex flex-col justify-center items-center">
+        <img :src="url"
+            >
+        <p>{{ nombre }}</p>
     </div>
 </template>
 <script>
 export default {
-
+    methods:{
+        async getImages(){
+            const images = await this.$axios.get()
+        }
+    },
+    props: {
+        nombre: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }
 }
 </script>
 <style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,32 @@
 <template>
-<div>
-  <Tutorial/>
-</div>
+    <div class="h-screen">
+        <div class="flex justify-center items center py-[1%]">
+            <p class="text-6xl font-bold">PokeDEx</p>
+        </div>
+        <div><input type="text" class="border-solid border-2 border-green-100 bg-green-50 mt-[2%]"></div>
+        <div class="my-[2%] flex flex-wrap px-[5%] h-[80%] ">
+            <p>{{ data }}</p>
+            <Card v-for="i in posts" :key="i" :nombre="i.name" :url="i.url" class="mx-[0.5%] my-[0.5%]"/>
+        </div>
+    </div>
 </template>
-
 <script>
-import Tutorial from '~/components/Tutorial.vue';
-
 export default {
-    name: "IndexPage",
-    components: { Tutorial }
-};
+    data() {
+        return {
+            posts: []
+        };
+    },
+    mounted(){
+        this.getInfo()
+    },
+    methods: {
+    async getInfo(){
+          const {data} = await  this.$axios.get('pokemon')
+          this.posts = data.results
+          console.log(data);
+        },
+    },
+}
 </script>
+<style></style>
